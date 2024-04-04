b0cb3f6f14eefeb7f7cde309366ee20ab4982eb2
Michal Debosz <michal.debosz2@sp.woodplc.com>
2024-04-04 12:39:44 +0200

    Added pages limit up to 10 files

--- pl-gov/app.py
+++ pl-gov/app.py
@@ -1,4 +1,5 @@
 import json
+import math
 import boto3
 from ast import literal_eval
 
@@ -12,20 +13,12 @@ storage = data["storage"]
 def composeResponse(code, body, msg='', ensure_ascii = False):
     return {
         'statusCode': code,
-        'body': {
-            "message": msg,
-            "data": json.dumps(body, ensure_ascii=ensure_ascii)
-            }
+        "headers" : {
+            "Content-Type" : "application/json"
+        },
+        'body': json.dumps(body, ensure_ascii=ensure_ascii)
+        
         }  
-
-def composeJsonResponse(code, body, msg=''):
-    return {
-        'statusCode': code,
-        'body': {
-            "message": msg,
-            "data": body
-            }
-        }
         
 def composePathToFile(storage, path, requestedFile):
     return storage + path + '/' + requestedFile
@@ -42,6 +35,9 @@ def checkIfEmptyString(text):
 def validList(text):
     return True if 'list' in text else False
     
+def validDeputiesVotes(text):
+    return True if 'deputiesVotes' in text else False
+    
 def validYear(text):
     return True if int(text) > 1989 else False
     
@@ -50,6 +46,10 @@ def validMonth(text):
     
 def validDay(text):
     return True if int(text) >= 1 and int(text) <= 31 else False
+    
+def validTermOfOffice(text):
+    terms = [7, 8, 9, 10]
+    return True if int(text) in terms else False
 
 def validPath(path):
     # /9/2023/08/16
@@ -59,6 +59,9 @@ def validPath(path):
     # ----------/16     -- Dzień
     if(validList(path) == True):
         path = path.replace('/list','')
+    
+    if(validDeputiesVotes(path) == True):
+        path = path.replace('/deputiesVotes','')
     
     path = '0'+path
     path_list = path.split("/")
@@ -75,16 +78,13 @@ def validPath(path):
     if length == 1:
         return False
     if length == 2:
-        return False
+        return True if validTermOfOffice(path_list[1]) else False
     if length == 3:
-        return False
+        return True if validTermOfOffice(path_list[1]) and validYear(path_list[2]) else False
     if length == 4:
-        return False
+        return True if validTermOfOffice(path_list[1]) and validYear(path_list[2]) and validMonth(path_list[3]) else False
     if length == 5:
-        if validYear(path_list[2]) and validMonth(path_list[3]) and validDay(path_list[4]):
-            return True
-        else:
-            return False
+        return True if validTermOfOffice(path_list[1]) and validYear(path_list[2]) and validMonth(path_list[3]) and validDay(path_list[4]) else False
 
 def getFileList(path , bucket = bucket, storage=storage, s3=s3):    
     prefix = storage + path + '/'
@@ -111,7 +111,6 @@ def getFiles(files, base_path, bucket=bucket, storage=storage):
         path = composePathToFile(storage, base_path, file)
         print(path)
         filesList.append(literal_eval(getFile(path))[0])
-    print (filesList)
     return filesList
     
 def replaceEmptyEventObject(eventObject, replacement, event):
@@ -119,14 +118,42 @@ def replaceEmptyEventObject(eventObject, replacement, event):
         requestedParameter = event[eventObject]
     except KeyError:
         requestedParameter = replacement
+    except TypeError:
+        requestedParameter = replacement
     finally:
-        if event is None:
-            return replacement
+        if ((event is None) or (requestedParameter is None)):
+            requestedParameter = replacement
     return requestedParameter
 
+def getFilesPerPages(files, event, page_size, path):
+    root = '/default/pl-gov'
+    total_pages = math.ceil(len(files) / page_size)
+    total_pages = 1 if total_pages == 0 else total_pages
+    page_num = int(replaceEmptyEventObject('queryStringParameters', {'page':1}, event)['page'])
+    
+    if page_num < 1 or page_num > total_pages:
+        return composeResponse(404, 'Page not found. ' + path)
+    
+    start = (page_num - 1) * page_size
+    end = min(page_num * page_size, len(files))
+    
+    next_link = None
+    if page_num < total_pages:
+        next_link = f"{root}{path}?page={page_num + 1}"
+    
+    prev_link = None
+    if page_num > 1:
+        prev_link = f"{root}{path}?page={page_num - 1}"
+        
+    return {
+            "files": files[start:end],
+            "next": next_link,
+            "previous": prev_link,
+            "total_pages": total_pages
+        }
     
 def lambda_handler(event, context):
-    
+    page_size = 10
     print('Event: ', event)
     path = clearPath(event['path'])
     try:
@@ -137,29 +164,33 @@ def lambda_handler(event, context):
     print(body)    
     requestedFile = replaceEmptyEventObject('file', '*', body)
     print(requestedFile)
-    
-    if(validList(path) == False and (path is not None) and (validPath(path) and (requestedFile != "*")) and (len(body['file']) == 1)):
-        # Return one file
-        path = composePathToFile(storage, path, body['file'][0])
-        return composeJsonResponse(200, getFile(path), 'Success')
         
-    elif(validList(path) == False and (path is not None) and (validPath(path) and (requestedFile != "*")) and (len(body['file']) > 1)):
+    if(validList(path) == False and (path is not None) and (validPath(path) and (requestedFile != "*")) and (len(body['file']) >= 1)):
         # Return multiple files
         files = getFiles(body['file'], path)
-        return composeResponse(200, files, 'Success')
+        #return composeResponse(200, files)
+        body = getFilesPerPages(files, event, page_size, path)
+        return composeResponse(200, body, True)
 
     elif(validList(path) == False and (path is not None) and (validPath(path) and (requestedFile == '*'))):
         # Return all files
         listFiles = getFileList(path)
         files = getFiles(listFiles, path)
-        return  composeJsonResponse(200, files, 'Success')
+        #return  composeResponse(200, files)
+        body = getFilesPerPages(files, event, page_size, path)
+        return composeResponse(200, body, True)
         
     elif(validList(path) == True and (path is not None) and validPath(path) ):
         # List files
         path = path.replace('/list','')
         listFiles = getFileList(path)
-        return  composeResponse(200, listFiles, 'List of files')
+        return  composeResponse(200, listFiles)
+        
+    elif(validList(path) == False and (path is not None) and path == "/"):
+        return composeResponse(200, {
+            "message":'I am alive, it is working.'
+        })
     
     else:
         print(validPath(path))
-        return composeResponse(400, path, 'Bad request. ')
+        return composeResponse(400, 'Bad request. ' + path)
