967682be1a74a4932706b7099751ec4a261cc3c0
Michal Debosz <michal.debosz2@sp.woodplc.com>
2024-04-04 15:40:09 +0200

    Updated API, added more endpoints, bug fixing

--- pl-gov/app.py
+++ pl-gov/app.py
@@ -20,9 +20,10 @@ def composeResponse(code, body, msg='', ensure_ascii = False):
         
         }  
         
-def composePathToFile(storage, path, requestedFile):
-    return storage + path + '/' + requestedFile
-
+def composePathToFiles(storage, path, requestedFiles):
+    text = storage + path + '/'
+    return [text + file for file in requestedFiles]
+    
 def clearPath(path):
     return path.replace('/pl-gov','') if ("/pl-gov" in path) else path
     
@@ -86,17 +87,14 @@ def validPath(path):
     if length == 5:
         return True if validTermOfOffice(path_list[1]) and validYear(path_list[2]) and validMonth(path_list[3]) and validDay(path_list[4]) else False
 
-def getFileList(path , bucket = bucket, storage=storage, s3=s3):    
-    prefix = storage + path + '/'
-    limiter='/'
-    
-    lista = s3.list_objects(Bucket = bucket, Prefix=prefix, Delimiter=limiter)['Contents']
-
-    list_of_files = []
-    for file in lista:
-        list_of_files.append(str.replace(file['Key'], prefix,''))     
-    list_of_files.remove('')
-    return list_of_files
+def getFileList(client, bucket, prefix=''):
+    prefix = 'data-pl-gov-votes' + prefix
+    response = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for content in response.get('Contents', []):
+        if content['Key'].endswith('/'):
+            getFileList(client, bucket, prefix=content['Key'])  # Recursive call for each found folder
+        else:
+            yield content['Key']
     
 def getFile(path, bucket = bucket):
     try:
@@ -104,13 +102,12 @@ def getFile(path, bucket = bucket):
     except:
         return {'path':path, 'message':'Not exists'}
 
-def getFiles(files, base_path, bucket=bucket, storage=storage):
+def getFiles(files, base_path='', bucket=bucket, storage=storage):
     print(files, base_path, bucket, storage)
     filesList = []
     for file in files:
-        path = composePathToFile(storage, base_path, file)
-        print(path)
-        filesList.append(literal_eval(getFile(path))[0])
+        filesList.append(literal_eval(getFile(file))[0])
+        
     return filesList
     
 def replaceEmptyEventObject(eventObject, replacement, event):
@@ -124,6 +121,18 @@ def replaceEmptyEventObject(eventObject, replacement, event):
         if ((event is None) or (requestedParameter is None)):
             requestedParameter = replacement
     return requestedParameter
+    
+def orderFiles(files):
+    deputiesVotes = []
+    old = []
+    for el in files:
+        if 'deputiesVotes' in el:
+            deputiesVotes.append(el)
+        else:
+            old.append(el)
+            
+    return deputiesVotes if old == [] else old
+    #return old + deputiesVotes
 
 def getFilesPerPages(files, event, page_size, path):
     root = '/default/pl-gov'
@@ -145,8 +154,11 @@ def getFilesPerPages(files, event, page_size, path):
     if page_num > 1:
         prev_link = f"{root}{path}?page={page_num - 1}"
         
+    files = orderFiles(files)
+    files = getFiles(files[start:end])
+        
     return {
-            "files": files[start:end],
+            "files": files,
             "next": next_link,
             "previous": prev_link,
             "total_pages": total_pages
@@ -167,24 +179,22 @@ def lambda_handler(event, context):
         
     if(validList(path) == False and (path is not None) and (validPath(path) and (requestedFile != "*")) and (len(body['file']) >= 1)):
         # Return multiple files
-        files = getFiles(body['file'], path)
-        #return composeResponse(200, files)
+        files = composePathToFiles(storage, path, body['file'])
         body = getFilesPerPages(files, event, page_size, path)
         return composeResponse(200, body, True)
 
     elif(validList(path) == False and (path is not None) and (validPath(path) and (requestedFile == '*'))):
         # Return all files
-        listFiles = getFileList(path)
-        files = getFiles(listFiles, path)
-        #return  composeResponse(200, files)
-        body = getFilesPerPages(files, event, page_size, path)
+        listFiles = list(getFileList(s3, bucket, prefix=path))
+        body = getFilesPerPages(listFiles, event, page_size, path)
         return composeResponse(200, body, True)
         
     elif(validList(path) == True and (path is not None) and validPath(path) ):
         # List files
         path = path.replace('/list','')
-        listFiles = getFileList(path)
-        return  composeResponse(200, listFiles)
+        listFiles = list(getFileList(s3, bucket, prefix=path))
+        listFiles = orderFiles(listFiles)
+        return  composeResponse(200, {"files":listFiles})
         
     elif(validList(path) == False and (path is not None) and path == "/"):
         return composeResponse(200, {
@@ -193,4 +203,4 @@ def lambda_handler(event, context):
     
     else:
         print(validPath(path))
-        return composeResponse(400, 'Bad request. ' + path)
+        return composeResponse(400, {"message":'Bad request. ', "path":path, "isValidPath":validPath(path)})
